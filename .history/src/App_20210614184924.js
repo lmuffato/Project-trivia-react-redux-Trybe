c31b7a0065a451a080dd44feb28a34800d228b0e
Juan Pablo <juan.tentiomaru9309@gmail.com>
2021-06-14 19:46:51 -0300

    feat:req-3 lint

--- .history/src/App_20210614184924.js
+++ .history/src/App_20210614184924.js
@@ -9,7 +9,7 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
-          <Routes />
+         <Routes />
         </p>
       </header>
     </div>
