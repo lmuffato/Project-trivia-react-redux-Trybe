65774b5cc57ea31e9c79701f30fc3f5f9e9d3f1e
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-06-14 17:24:01 -0300

    feat: req01

---
+++ src/pages/Login.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      buttonEnable: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.checkInputs = this.checkInputs.bind(this);
+  }
+
+  checkInputs() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    if (name && email) {
+      this.setState({ buttonEnable: false });
+    } else {
+      this.setState({ buttonEnable: true });
+    }
+  }
+
+  handleChange(event) {
+    const { target: { name, value } } = event;
+    this.setState({ [name]: value });
+    this.checkInputs();
+  }
+
+  render() {
+    const { email, name, buttonEnable } = this.state;
+    return (
+      <form>
+        <label htmlFor="name">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            data-testid="input-player-name"
+            onChange={ this.handleChange }
+            value={ name }
+          />
+        </label>
+        <label htmlFor="email">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            data-testid="input-gravatar-email"
+            onChange={ this.handleChange }
+            value={ email }
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="btn-play"
+          disabled={ buttonEnable }
+        >
+          Jogar
+        </button>
+      </form>
+    );
+  }
+}
