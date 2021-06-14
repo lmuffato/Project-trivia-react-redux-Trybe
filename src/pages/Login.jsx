1ca710ebe04e8979349e11521dfd480c595a2c76
Gui-lira <gui.lira.martins@gmail.com>
2021-06-14 18:15:13 -0300

    requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  const handleEmail = (evt) => (
+    setEmail(evt.target.value)
+  );
+
+  const handleName = (evt) => (
+    setName(evt.target.value)
+  );
+
+  return (
+    <div>
+      <form>
+        <input
+          type="email"
+          value={ email }
+          onChange={ handleEmail }
+          data-testid="input-gravatar-email"
+        />
+        <input
+          type="text"
+          value={ name }
+          onChange={ handleName }
+          data-testid="input-player-name"
+        />
+        <button
+          disabled={ email.length === 0 || name.length === 0 }
+          type="submit"
+          data-testid="btn-play"
+        >
+          Jogar
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ src/redux/reducer/loginReducer.js
@@ -0,0 +1,5 @@
+const INITIAL_STATE = null;
+
+export default function loginReducer(state = INITIAL_STATE) {
+  return state;
+}
