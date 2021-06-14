23a71e41272ee33697f04f00ac73ee1778083dc5
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-06-14 19:32:41 -0300

    feat: requisito 4

---
+++ src/Pages/Feedback.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Componentes/Header';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Feedback;
