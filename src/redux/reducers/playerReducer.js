2b08b63dada6af780936c71b8bf177a26ba186bf
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-06-14 17:43:02 -0300

    feat: redux files

---
+++ src/redux/reducers/playerReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+
+};
+
+const playReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default playReducer;
