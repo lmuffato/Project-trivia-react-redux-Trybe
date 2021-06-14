174134dd19fb70f664f7d6db077d57a3074ae9ce
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-06-14 17:56:12 -0300

    feat: configuraçao redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
 
-export default store = createStore(rootReducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+export default store;
