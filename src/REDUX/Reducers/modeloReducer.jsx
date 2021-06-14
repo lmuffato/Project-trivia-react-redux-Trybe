2a2366135a292aefd6042b8678584e481376fa13
hheloisa <hhackenhaar@gmail.com>
2021-06-14 16:25:20 -0300

    monta a estrutura inicial do redux

---
+++ src/REDUX/Reducers/modeloReducer.jsx
@@ -0,0 +1,15 @@
+/* import { SELECT_MOVIE } from '../actions/index';
+import categories from '../data';
+const INITIAL_STATE = {
+};
+export default function movieReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SELECT_MOVIE:
+    return {
+      state,
+    };
+  default:
+    state;
+  }
+}
+ */
