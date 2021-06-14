232bbf2be3c2fa832fa075bfeea3d2bfffcde6f2
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-06-14 18:29:05 -0300

    :rocket: Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import Login from './components/Login';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
+        SUA VEZ
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
       </header>
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { handleChangeUser } from '../actions/actionUser';
+
+class Login extends Component {
+  render() {
+    const { name, email, handleChange } = this.props;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              data-testid="input-gravatar-email"
+              onChange={ handleChange }
+              value={ email }
+            />
+          </label>
+          <label htmlFor="name">
+            Nome:
+            <input
+              type="text"
+              id="name"
+              name="name"
+              data-testid="input-player-name"
+              onChange={ handleChange }
+              value={ name }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="btn-play"
+            disabled={ !!(!name || !email) }
+          >
+            Jogar
+
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  name: state.user.name,
+  email: state.user.email,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  handleChange: ({
+    target: { name, value },
+  }) => dispatch(handleChangeUser(name, value)),
+});
+
+Login.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
