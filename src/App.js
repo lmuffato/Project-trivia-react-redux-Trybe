7c1bcb17a1a621ec97a74c788a5b891c0ba576e1
Perycles reis <perycles_reis@hotmail.com>
2021-06-14 18:24:21 -0300

    erro

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
     </div>
   );
 }
