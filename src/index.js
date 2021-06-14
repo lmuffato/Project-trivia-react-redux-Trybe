47f15ad16655343632a1ff9916f64cbd505fd315
MaxRudim <max.hermsdorff@gmail.com>
2021-06-14 18:39:22 -0300

    Req. 01 concluido

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
