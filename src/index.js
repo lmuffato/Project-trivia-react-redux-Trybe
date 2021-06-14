6079c387d15aeb4f732a3a16bf019263da0ac659
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-06-14 20:31:17 -0300

    clear lint req 1

--- src/index.js
+++ src/index.js
@@ -12,8 +12,9 @@ ReactDOM.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
-, document.getElementById('root'));
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
