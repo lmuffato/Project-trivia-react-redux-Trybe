e583e1fcdabbf965585342c78728588607c21122
Alexandre Oliveira <alexandremolive@gmail.com>
2021-06-14 19:03:44 -0300

    requisito 2 alteracoes no link da pagina

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Game from './pages/Game';
 
 export default function App() {
   return (
+
     <Switch>
       <Route path="/game" component={ Game } />
       <Route exact path="/" component={ Login } />
