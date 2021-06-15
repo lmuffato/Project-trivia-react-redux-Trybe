1d2eebca990e78c095717889042a3e47039f4730
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-15 13:40:06 -0400

    alterado caminho ao clicar no botao jogar e adicionado a rota da pagina gameplay

---
+++ src/pages/GamePlay.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class GamePlay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    return (
+      <h1>Gameplay</h1>
+    );
+  }
+}
+
+export default GamePlay;
