6903740be704347ba7c922a4a0794ef55830c435
lucas-paz <lucas.hpaz@outlook.com>
2021-06-15 12:43:50 -0300

    creates the header body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
+    const { name, score, gravatar } = this.props;
     return (
       <header>
-        Header do Trivia
+        <img src={ gravatar } alt="user" data-testid="header-profile-picture" />
+        <span data-testid=" header-player-name ">{ name }</span>
+        <span data-testid="header-score">{ score }</span>
       </header>
     );
   }
 }
 
-// Header.propTypes = {
+Header.propTypes = {
 
-// }.isRequired;
+}.isRequired;
 
 export default Header;
