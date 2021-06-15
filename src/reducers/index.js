49713d98b16d687d78c2e0b7fc0c8d65227d0d36
guidugaich <guidugaich.dev@gmail.com>
2021-06-15 19:16:48 -0300

    fix: changing reducer name

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import questionGame from './reducerQestion';
+import questions from './questions';
 
 const rootReducer = combineReducers({
-  questionGame,
+  questions,
 });
 
 export default rootReducer;
