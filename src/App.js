d4ca7f939334d28f97249a27edfe8052a7438e75
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-06-15 10:15:29 -0300

    (req2) add token request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
+import GameScreen from './pages/GameScreen';
 import Login from './pages/Login';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/gamescreen" component={ GameScreen } />
     </Switch>
   );
 }
