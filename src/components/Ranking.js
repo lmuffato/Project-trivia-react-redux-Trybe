65dab5c71f7095438870acca1298faf9c3d1782e
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-06-15 17:35:37 -0300

    requisito 18 feito

---
+++ src/components/Ranking.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Link from 'react-router-dom';
+
+class Ranking extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="/" data-testid="btn-go-home">
+          Voltar ao início
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Ranking;
