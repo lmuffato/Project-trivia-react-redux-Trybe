03a84def6d0363b93b09387aabaa187aaaf2eab5
Renata Andrade <reapminas@gmail.com>
2021-06-15 15:17:41 -0300

    feat: :rocket: requisito 03

---
+++ src/pages/Game.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <section />
+    );
+  }
+}
+
+export default Game;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Settings from '../components/Settings';
 
 export default class Login extends Component {
   constructor() {
@@ -34,6 +35,13 @@ export default class Login extends Component {
 
     return (
       <section>
+        <button
+          type="button"
+          data-testid="btn-settings"
+          className="bi bi-gear-fill"
+          aria-label="Configurações"
+        />
+        <Settings />
         <label htmlFor="player-name">
           Nome:
           <input
