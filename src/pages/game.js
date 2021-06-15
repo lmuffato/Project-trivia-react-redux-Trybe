7706e70564739e3b5f9ebbe8a5f5e9bdae17f21b
rcalvs <rafaelcalvette@gmail.com>
2021-06-15 15:34:47 -0300

    goup-32-req-4

--- src/pages/game.js
+++ src/pages/game.js
@@ -4,9 +4,19 @@ class Game extends Component {
   render() {
     return (
       <div>
-        OLá mundo, aqui é a página de Game
-        Seu token é esse
-        { localStorage.token }
+        <header>
+          <img
+            data-testid="header-profile-picture"
+            src={ localStorage.userImg }
+            alt="Foto do Usuario"
+          />
+          <h3 data-testid="header-player-name">{ localStorage.usuario }</h3>
+          <p data-testid="header-score">0</p>
+          <p>
+            sua sessão é
+            { localStorage.token }
+          </p>
+        </header>
       </div>
     );
   }
--- src/services/API.js
+++ src/services/API.js
@@ -1,9 +1,17 @@
-// const requestAPI = 'https://opentdb.com/api_token.php?command=request'
+import { MD5 } from 'crypto-js';
 
-const token = () => {
+export const token = () => {
   fetch('https://opentdb.com/api_token.php?command=request')
     .then((response) => response.json())
     .then((result) => localStorage.setItem('token', result.token));
 };
 
-export default token;
+export const emailHash = (usuario, email) => {
+  // return console.log(usuario, email)
+  const hash = MD5(email).toString();
+  const linkGravatar = `https://www.gravatar.com/avatar/${hash}`;
+  localStorage.setItem('userImg', linkGravatar);
+  localStorage.setItem('usuario', usuario);
+  return console.log(localStorage);
+};
+// export default token;
