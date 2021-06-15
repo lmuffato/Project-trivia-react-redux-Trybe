5585e1cd447b13585f3548d53a8778dad950918e
Maria Baeta <m.martinsdg@gmail.com>
2021-06-15 17:10:33 -0300

    Corrige erros do lint GitHub

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import Login from './Pages/Login';
 import Feedback from './Pages/Feedback';
 import Settings from './Pages/Settings';
 
-export default function App() {
-  return (
-    <div className="App">
-      <Switch>
-        {/* Atenção: Utilizar SEMPRE essa estrutura de Route */}
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route path="/game">
-          <Game />
-        </Route>
-        <Route path="/feedback">
-          <Feedback />
-        </Route>
-        <Route path="/settings">
-          <Settings />
-        </Route>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          {/* Atenção: Utilizar SEMPRE essa estrutura de Route */}
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+          <Route path="/feedback">
+            <Feedback />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
+export default App;
