383c9c544e327089064defba1c7a4cd4657f9e3f
lucas-paz <lucas.hpaz@outlook.com>
2021-06-15 17:59:00 -0300

    start the ranking screen

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Ranking extends Component {
   render() {
-    const { points } = this.props;
     return (
-      <header>
-        
-      </header>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   points: state.player.score,
+  gravatar: state.player.gravatar,
+  name: state.player.name,
 });
 
-// Ranking.propTypes = {
-
-// };
+Ranking.propTypes = {
+  points: PropTypes.number,
+  gravatar: PropTypes.string,
+}.isRequired;
 
 export default connect(mapStateToProps, null)(Ranking);
