613edad4004b119ccf6948ed10506545466a5258
Sergio Martins <sergiomos2010@gmail.com>
2021-06-15 16:47:08 -0300

    Feat: req15 - play again button

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import FeedbackHeader from '../components/FeedbackHeader';
 import FeedbackMsg from '../components/FeedbackMsg';
 import feedbackResult from '../components/feedbackResult';
@@ -10,6 +11,12 @@ export class Feedback extends Component {
         <FeedbackHeader />
         <FeedbackMsg />
         <feedbackResult />
+        <Link 
+          to="/"
+          data-testid="btn-play-again"
+        >
+          Jogar novamente
+        </Link>
       </div>
     )
   }
