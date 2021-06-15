d667eefa779c802d97d9df3bbb0811577bbc3f11
Arlesson Moura <arlessonmss@gmail.com>
2021-06-15 16:28:03 -0300

    Feat: Requisito 07, Arlesson Moura e Rafael Medeiros

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
 
+// Creio que meu requesito seja aqui
+
 class Game extends Component {
   render() {
     return (
