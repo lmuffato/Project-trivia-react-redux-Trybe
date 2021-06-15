90dbbbb1238c94e3874b507bdd856b92536a409a
Flavio Franco <flavio.win@gmail.com>
2021-06-14 21:03:04 -0300

    req 5 inacabado

--- src/pages/TelaDoJogo.js
+++ src/pages/TelaDoJogo.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { getQuestions } from '../services/api';
 import Header from '../components/Header';
 
 class TelaDoJogo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: [],
+      finishRequest: false,
+    };
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  async componentDidMount() {
+    const response = await getQuestions();
+    this.updateState(response);
+  }
+
+  updateState(response) {
+    this.setState({
+      results: response,
+    }, () => this.setState({
+      finishRequest: true,
+    }));
+  }
+
   render() {
+    const { results, finishRequest } = this.state;
     return (
       <div>
         <Header />
+        {finishRequest && results
+          .map(({ category, question, correct_answer, incorrect_answers }) => {
+            const allAnsewers = [...incorrect_answers, correct_answer];
+            return (
+              <div>
+                <p data-testid="question-category">{category}</p>
+                <p data-testid="question-text">{question}</p>
+                { allAnsewers.map((answers) => <p>{answers}</p>)}
+              </div>
+            );
+          })}
+
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,11 @@ export const apiTrivia = async () => {
   return token;
 };
 
+export const getQuestions = async () => {
+  const token = await apiTrivia();
+  const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+  const json = await response.json();
+  return json.results;
+};
+
 export default apiTrivia;
