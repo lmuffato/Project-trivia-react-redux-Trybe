ba7defcd9759d0439c0bb9d49f8c92d16bd4c178
Marcus C. H. Filho <mchusemann@gmail.com>
2021-06-15 17:27:49 -0300

    feat(Requirement-13): verifica pontuaçao com ternario (score mockado)

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,8 +3,17 @@ import Header from '../components/Header';
 
 class App extends Component {
   render() {
+    const score = 2;
+    const minScore = 3;
     return (
-      <Header />
+      <>
+        <Header />
+        <section>
+          {(score < minScore)
+            ? <span data-testid="feedback-text"> Podia ser melhor... </span>
+            : <span data-testid="feedback-text"> Mandou bem! </span> }
+        </section>
+      </>
     );
   }
 }
