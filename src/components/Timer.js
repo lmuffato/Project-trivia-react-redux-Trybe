1268c3cce4a044fcd13f19e56bb3e8eb037719d3
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-06-15 16:29:53 -0300

    requisito 10

---
+++ src/components/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const time = 30;
+
+const timer = (time) => {
+    setInterval((
+        time -= 1;
+        if (time > 0){
+            timer(time);
+        }
+    ),1000);
+};
