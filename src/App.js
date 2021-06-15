fcca21c3ad7d71452a28d0829944baa65dc2bd37
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-06-15 15:02:16 -0300

    Fix: evaluator: page home excluded

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
 
-import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route path="/login" component={ Login } />
+      <Route exact path="/" component={ Login } />
       <Route component={ NotFound } />
     </Switch>
   );
