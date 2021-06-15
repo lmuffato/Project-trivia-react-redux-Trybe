7e6e9786a945a72312c0dc0b023a93b9b25b3e7d
renancosta21 <renancosta21@gmail.com>
2021-06-15 11:36:06 -0300

    feat: req4 implementado

--- src/pages/Jogo.jsx
+++ src/pages/Jogo.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
 import { getQuestions } from '../services/triviaAPI';
 
-export default class Jogo extends Component {
+class Jogo extends Component {
+  renderGravatarImage() {
+    const { email } = this.props;
+    const hashMD5 = md5(email).toString();
+    return (
+      <img
+        src={ `https://www.gravatar.com/avatar/${hashMD5}` }
+        alt="avatar"
+        data-testid="header-profile-picture"
+      />);
+  }
+
   render() {
+    const { nome } = this.props;
     console.log(getQuestions());
     return (
       <div>
+        <header>
+          {this.renderGravatarImage()}
+          <span data-testid="header-player-name">{ nome }</span>
+          <span data-testid="header-score">0</span>
+        </header>
         <h1>Página do Jogo</h1>
       </div>
     );
   }
 }
+
+Jogo.propTypes = {
+  email: PropTypes.string,
+  nome: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.loginReducer.email,
+  nome: state.loginReducer.nome,
+});
+
+export default connect(mapStateToProps, null)(Jogo);
