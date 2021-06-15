432095061fb2198d4e8edfa5a12a09c33a6187c9
Samuel <samuelvanguarda@gmail.com>
2021-06-15 17:41:42 -0300

    Fix: Req 17 e 18

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
 class Ranking extends Component {
+  constructor() {
+    super();
+    this.renderImgGravatar = this.renderImgGravatar.bind(this);
+  }
+
+  // cria uma lista com as informações de cada jogador com as informações do estado global
+  renderImgGravatar() {
+    const gravatarImg = localStorage.getItem('token');
+    console.log(gravatarImg);
+  }
+
   render() {
     return (
       <div>
-        <ul>
-          <li />
-          <li data-testid={ `player-name-${index}` } />
-          <li data-testid={ `player-score-${index}` } />
-        </ul>
+        { this.renderImgGravatar }
         <Link to="/">
           <button data-testid="btn-go-home" type="button">Voltar ao início</button>
         </Link>
       </div>
     );
   }
+// Requisito 18 - Botão usado para retornar ao início;
 }
+// Pegando o estado Global
+/* const mapStateToProps = (state) => ({
+  stateGotten: state.userReducer,
+});
 
-export default Ranking;
+Ranking.propTypes = {
+  stateGotten: PropTypes.arrayOf(PropTypes.object).isRequired,
+}; */
+
+export default connect(mapStateToProps)(Ranking);
