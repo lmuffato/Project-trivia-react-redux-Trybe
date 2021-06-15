1cd7e9c1ba62ec7bb7ab1745d6d49272d28f3a5d
Ana Ventura <anaventura1811@gmail.com>
2021-06-15 16:15:42 -0300

    Configura action para salvar nome do usuáriono store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
 export const USER_LOGIN = 'USER_LOGIN';
+export const USER_NAME = 'USER_NAME';
 
 export const loginAction = (payload) => ({ type: USER_LOGIN, payload });
+export const userNameAction = (payload) => ({ type: USER_NAME, payload });
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,13 +1,16 @@
-import { USER_LOGIN } from '../actions';
+import { USER_LOGIN, USER_NAME } from '../actions';
 
 const INITIAL_STATE = {
   email: '',
+  user: '',
 };
 
 const userReducer = (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
   case USER_LOGIN:
     return { ...state, email: payload };
+  case USER_NAME:
+    return { ...state, user: payload };
   default:
     return state;
   }
