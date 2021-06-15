cfaa6ea020d4943d8727b57cd811b3cfb8cc100f
Lucas André <lucas.cancado89@gmail.com>
2021-06-15 19:06:45 -0300

    Req 15 done

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
-    const { assertions } = localStorage.getItem('player');
+    localStorage.setItem('player', JSON.stringify({ assertions: 2 }));
+    const { assertions } = JSON.parse(localStorage.getItem('player'));
     const goodAssertions = 3;
     return (
       <div>
@@ -11,9 +23,20 @@ class Feedback extends React.Component {
             ? <p data-testid="feedback-text">Podia ser melhor...</p>
             : <p data-testid="feedback-text">Mandou bem!</p>
         }
+        <button
+          onClick={ this.handleClick }
+          data-testid="btn-play-again"
+          type="button"
+        >
+          Jogar novamente
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.objectOf(PropTypes.func).isRequired,
+};
 
 export default Feedback;
