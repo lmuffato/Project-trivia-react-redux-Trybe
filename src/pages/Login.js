ff0c6eab7250b65429302b8145aa5b071da2b109
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-06-14 22:45:03 -0300

    fix: Requirement 02,PropTypes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { fetchToken } from '../redux/actions';
 
 class Login extends React.Component {
@@ -74,5 +75,9 @@ class Login extends React.Component {
 const mapDispatchToProps = (dispatch) => ({
   loginAction: () => dispatch(fetchToken()),
 });
+
+Login.propTypes = {
+  loginAction: PropTypes.func,
+}.isRequired;
 
 export default connect(null, mapDispatchToProps)(Login);
