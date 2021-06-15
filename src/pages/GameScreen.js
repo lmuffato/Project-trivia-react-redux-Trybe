2fc1fb01efb0b61d8e7af4fc59690fa788198fe3
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-06-15 14:09:39 -0300

    add files

---
+++ src/pages/GameScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class GameScreen extends React.Component {
+  render() {
+    return (<p>Game Screen</p>);
+  }
+}
+
+export default GameScreen;
---
+++ src/pages/SetupScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SetupScreen extends React.Component {
+  render() {
+    return (
+      <p data-testid="settings-title">Setup Screen</p>
+    );
+  }
+}
+
+export default SetupScreen;
