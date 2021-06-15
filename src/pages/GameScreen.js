862557315cb7426c99fb532edb42f334c175b0c5
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-06-15 16:48:54 -0400

    feat: gameScreen

--- src/pages/GameScreen.js
+++ src/pages/GameScreen.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class GameScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: {},
+    };
+    this.mockApi = this.mockApi.bind(this);
+  }
+
+  componentDidMount() {
+    this.mockApi();
+  }
+
+  async mockApi() {
+    let { APItoken } = this.props;
+    APItoken = '9965ae223e2bc65f25134bb13e3f99045e0df2d2b668450da4a676fe8b8e898a';
+    const getAPIQuestions = await fetch(`https://opentdb.com/api.php?amount=5&token=${APItoken}`);
+    const questions = await getAPIQuestions.json();
+    this.setState({ questions });
+  }
+
   render() {
     return (
       <section>
@@ -10,4 +31,8 @@ class GameScreen extends React.Component {
   }
 }
 
-export default GameScreen;
+const mapStateToProps = (state) => ({
+  APItoken: state.login.token.token,
+});
+
+export default connect(mapStateToProps, null)(GameScreen);
