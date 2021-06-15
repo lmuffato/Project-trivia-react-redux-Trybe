7d985c188a7752f91865af647480afc97d4fcb1e
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-15 11:52:44 -0400

    req 3 completo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Feedback from './pages/Feedback';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/settings" component={ Settings } />
       </Switch>
     </div>
   );
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <h1 data-testid="settings-title">
+        Página de configurações
+      </h1>
+    );
+  }
+}
+
+export default Settings;
