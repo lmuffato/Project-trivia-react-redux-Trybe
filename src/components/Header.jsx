c3ea8e966fd3cb287733bd53caf89e990f9a2966
lucas-paz <lucas.hpaz@outlook.com>
2021-06-15 12:35:26 -0300

    create component header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        Header do Trivia
+      </header>
+    );
+  }
+}
+
+// Header.propTypes = {
+
+// }.isRequired;
+
+export default Header;
