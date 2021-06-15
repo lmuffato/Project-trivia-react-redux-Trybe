bdc8f90d9b8ab00fe3e818c4af9e08559420b705
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-06-14 23:49:09 -0300

    requisito 5 impl

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Questions from '../components/Questions';
 
 export default class Game extends Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Questions />
+      </>
     );
   }
 }
