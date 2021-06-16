130a756c1e17eba5ca55c1edbe767d24ffeb2b7d
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-06-16 20:06:23 -0300

    lint correction

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 class Feedback extends React.Component {
   render() {
     return (
-      <>
       <h1 data-testid="feedback-text">
         Feedback
-        </h1>
-      </>
+      </h1>
     );
   }
 }
