9a83cf239524ed41fc0b8ab954ee6c12ca0e1de0
ThalitaC <thalitaceci@gmail.com>
2021-06-16 15:14:58 -0300

    req18 done

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
+//   constructor() {
+//     super();
+//     this.ranking = this.ranking.bind(this);
+//   }
+
+  // const gravatar = localStorage.getItem('token');
+
   render() {
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div>
+        <Link to="/">
+          <button
+            data-testid="btn-go-home"
+            type="button"
+          >
+            Voltar para tela inicial
+          </button>
+        </Link>
+        Ranking
+      </div>
     );
   }
 }
