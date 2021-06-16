c0049e80c9b834451e4ead2daca133b635b81e32
Lucas Pedroso <lepsk8@gmail.com>
2021-06-16 14:24:26 -0300

    feat: add page Ranking

---
+++ src/pages/Ranking.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export const Ranking = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Ranking.propTypes = {
+  props: PropTypes
+}
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Ranking)
