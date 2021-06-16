b6bf707363b78f7574774626f76a474c0d8cc382
rcalvs <rafaelcalvette@gmail.com>
2021-06-16 12:46:35 -0300

    salvando pequenas alteraçoes

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <img
+            data-testid="header-profile-picture"
+            src={ localStorage.userImg }
+            alt="Foto do Usuario"
+          />
+          <h3 data-testid="header-player-name">{ localStorage.usuario }</h3>
+          <p data-testid="header-score">0</p>
+          <p>
+            sua sessão é
+            { localStorage.token }
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/pages/ranking.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from '../components/questions';
+import { Link } from 'react-router-dom';
+
+class Ranking extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+          OLá mundo, aqui é a págiana de Ranking
+        <Link to="/game">
+          <button type="button" data-testid="btn-go-home">Jogue Denovo</button>
+          {/* provavelmente quando clicar no botão será necessario rodar a API novamente */}
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Ranking;
