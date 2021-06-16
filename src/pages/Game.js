90f0dbfc09958edb5c7fc3e4fbc69a6df88a9774
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-06-16 02:09:48 -0300

    fix: lint req10

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,65 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import ApiGetQuestions from '../services/ApiGetQuestions';
-// import Loading from '../components/Loading';
+import PropTypes from 'prop-types';
 import styles from './game.module.css';
 import Header from '../components/Header';
 import Questions from '../components/Questions';
 import { getQuestionsThunk } from '../redux/actions';
 
 class Game extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      questions: [],
-      questionsIndex: 0,
-      loading: true,
-    };
-    // this.getApi = this.getApi.bind(this);
-  }
-
   componentDidMount() {
-    // this.getApi();
     const { getQuestions } = this.props;
     getQuestions();
   }
 
-  // async getApi() {
-  //   const questions = await ApiGetQuestions();
-  //   this.setState({ questions, loading: true }, () => {
-  //     this.setState({ loading: false });
-  //   });
-  // }
-
-  nextQuestion(NumberOfQuestions) {
-    this.setState((prevState) => ({
-      questionsIndex: (prevState.questionsIndex + 1) % NumberOfQuestions,
-    }));
-  }
-
   render() {
-    // const token = localStorage.getItem('token');
-    // const { questions, questionsIndex, loading } = this.state;
-    // const { questionsTest, loadingTest } = this.props;
-    // console.log(questionsTest, loadingTest);
-    // const questionsFiltered = questions[questionsIndex];
-    // if (loading) {
-    //   return <Loading />;
-    // }
     return (
       <>
         <Header />
         <div className={ styles.question__container }>
-          {/* <Questions questionsFiltered={ questionsFiltered } /> */}
           <Questions />
-          {/* <button
-            type="button"
-            onClick={ () => this.nextQuestion(questions.length) }
-            className={ styles.question__button }
-          >
-            Next
-          </button> */}
-
         </div>
       </>
     );
@@ -71,3 +29,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(null, mapDispatchToProps)(Game);
+
+Game.propTypes = {
+  getQuestions: PropTypes.func.isRequired,
+};
