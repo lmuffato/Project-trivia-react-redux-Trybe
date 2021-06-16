a9221554e5f89c5f4f37aa0f094e06549449744a
Gabriel Ferreira <glucas709@gmail.com>
2021-06-16 17:32:48 -0300

    feat: requisito 7

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -9,12 +9,23 @@ class Questions extends React.Component {
   constructor() {
     super();
     this.getID = this.getID.bind(this);
+    this.checkAnswer = this.checkAnswer.bind(this);
   }
 
   getID(answer) {
     const { questions } = this.props;
     if (answer === questions[0].correct_answer) return 'correct-answer';
     return `wrong-answer-${questions[0].incorrect_answers.indexOf(answer)}`;
+  }
+
+  checkAnswer({ target: { parentElement } }) {
+    Array.from(parentElement.children).forEach((child) => {
+      if (child.id === 'correct-answer') {
+        child.className = 'answer correct';
+      } else {
+        child.className = 'answer wrong';
+      }
+    });
   }
 
   render() {
@@ -29,16 +40,27 @@ class Questions extends React.Component {
     return (
       <section>
         <h1>Trivia Game!</h1>
-        <h3 data-testid="question-category">{category}</h3>
-        <h4 data-testid="question-text">{questionDecoded}</h4>
-        {permutate(...answers).map((answer, index) => {
-          const answerDecoded = decoder(answer);
-          return (
-            <button type="button" data-testid={ this.getID(answer) } key={ index }>
-              {answerDecoded}
-            </button>
-          );
-        })}
+        <div className="game-container">
+          <h3 data-testid="question-category">{category}</h3>
+          <h4 data-testid="question-text">{questionDecoded}</h4>
+          <div className="answers-container">
+            {permutate(...answers).map((answer, index) => {
+              const answerDecoded = decoder(answer);
+              return (
+                <button
+                  type="button"
+                  className="answer"
+                  data-testid={ this.getID(answer) }
+                  id={ this.getID(answer) }
+                  key={ index }
+                  onClick={ this.checkAnswer }
+                >
+                  {answerDecoded}
+                </button>
+              );
+            })}
+          </div>
+        </div>
       </section>
     );
   }
