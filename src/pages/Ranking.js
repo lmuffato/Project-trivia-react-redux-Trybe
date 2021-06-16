906be0d9a3fde97bfd1d19c399b76eb7aad4b456
ThalitaC <thalitaceci@gmail.com>
2021-06-16 15:45:57 -0300

    fix testid

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -21,7 +21,7 @@ class Ranking extends React.Component {
     const { gravatar } = this.state;
     return (
       <div>
-        <h1>Ranking</h1>
+        <h1 data-testid="ranking-title">Ranking</h1>
         <img
           src={ gravatar }
           alt="User"
