ca707b4916e0d76777bde63d0ae0e02497bec546
Arlesson Moura <arlessonmss@gmail.com>
2021-06-15 23:21:42 -0300

    Feat: requirement 07. Arlesson And William.

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import TriviaGame from '../components/TriviaGame';
 
-// Creio que meu requesito seja aqui
-
 class Game extends Component {
   render() {
     return (
