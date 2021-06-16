4580e5c691a4ed30335d124f6fb20b26ef2193f3
igson <igson660@gmail.com>
2021-06-16 14:14:20 -0500

    fix: refactor req8

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,12 +1,6 @@
 export const LOGIN = 'LOGIN';
-export const TIME = 'TIME';
 
 export const login = (payload) => ({
   type: LOGIN,
-  payload,
-});
-
-export const disableButton = (payload) => ({
-  type: TIME,
   payload,
 });
