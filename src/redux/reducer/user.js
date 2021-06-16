6b49015728e0a63be8d64fec5eefc2871f32f105
Lucas Pedroso <lepsk8@gmail.com>
2021-06-16 14:30:31 -0300

    feat(reducer): add case IS_FETCHING and GET_QUESTIONS

--- src/redux/reducer/user.js
+++ src/redux/reducer/user.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN } from '../action';
+import { GET_QUESTIONS, IS_FETCHING, USER_LOGIN } from '../action';
 
 const initialState = {
   email: '',
@@ -9,7 +9,10 @@ export default (state = initialState, { type, payload }) => {
   switch (type) {
   case USER_LOGIN:
     return { ...state, ...payload };
-
+  case IS_FETCHING:
+    return { ...state, isFetching: true };
+  case GET_QUESTIONS:
+    return { ...state, questions: payload };
   default:
     return state;
   }
