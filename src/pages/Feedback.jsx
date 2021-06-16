c139646ec44f32ef77574bbfedba01a8866a3c76
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-06-16 20:41:02 -0300

    req 15 impl

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Feedback extends React.Component {
   render() {
     return (
-      <h1 data-testid="feedback-text">
-        Feedback
-      </h1>
+      <>
+        <h1 data-testid="feedback-text">
+          Feedback
+        </h1>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-play-again"
+          >
+            Tela Inicial
+          </button>
+        </Link>
+      </>
     );
   }
 }
