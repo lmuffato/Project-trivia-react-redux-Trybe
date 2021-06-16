57148c58800f05bb55ef5d982a7abbf5742d9d0b
Lucas Pedroso <lepsk8@gmail.com>
2021-06-16 14:27:56 -0300

    feat(components): add Question

---
+++ src/components/Question.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+function Question(props) {
+  const { questions, index } = props;
+  console.log('QUESTOES', questions);
+  return (
+    <div className="question">
+      <div><p data-testid="question-category">{ questions[index].category }</p></div>
+    </div>
+  );
+}
+
+Question.propTypes = {
+  questions: PropTypes.arrayOf().isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  questions: state.user.questions,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Question);
