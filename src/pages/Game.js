b52c77f24169d90c2864d41e6312efb87797dae0
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-06-16 16:34:56 -0300

    Fix: (req5) finalize and Feat:(req6 and req7) implements

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -8,35 +8,52 @@ class Game extends React.Component {
     super(props);
     this.state = {
       quests: [],
+      alternativeRandom: [],
       indice: 0,
       loading: true,
+      userAnswer: false,
     };
     this.requestApi = this.requestApi.bind(this);
     this.renderQuestions = this.renderQuestions.bind(this);
+    this.ramdomizeQuestions = this.ramdomizeQuestions.bind(this);
   }
 
   componentDidMount() {
     this.requestApi();
   }
 
-  async requestApi() {
+  requestApi() {
     const token = JSON.parse(localStorage.getItem('token'));
-    const res = await api.fetchQuest(token);
-    this.setState({
-      quests: res.results,
-      loading: false,
+    api.fetchQuest(token).then((res) => {
+      this.setState({
+        quests: res.results,
+        loading: false,
+      });
+      this.ramdomizeQuestions();
     });
-    console.log(res);
   }
 
-  renderQuestions() {
+  changeAnswer(alternative, crrQuestion) {
+    return alternative === crrQuestion.correct_answer
+      ? 'ok' : 'fail';
+  }
+
+  ramdomizeQuestions() {
     const { quests, indice } = this.state;
     const crrQuestion = quests[indice];
     const altenativesOld = crrQuestion.incorrect_answers
       .concat(crrQuestion.correct_answer);
     const numMagic = 0.5;
     const alternatives = altenativesOld.sort(() => Math.random() - numMagic);
+    this.setState(() => ({
+      userAnswer: false,
+      alternativeRandom: alternatives,
+    }));
+  }
 
+  renderQuestions() {
+    const { quests, indice, userAnswer, alternativeRandom } = this.state;
+    const crrQuestion = quests[indice];
     return (
       <div>
         <Header />
@@ -46,26 +63,30 @@ class Game extends React.Component {
         <p data-testid="question-text">
           {crrQuestion.question}
         </p>
-        {alternatives.map((alternative, index) => (
+        {alternativeRandom.map((alternative, index) => (
           <button
+            disabled={ userAnswer }
             type="button"
+            value={ alternative }
             key={ Math.random() }
-            onClick={
-              () => this.setState((prevState) => ({ indice: prevState.indice + 1 }))
-            }
+            onClick={ () => (this.setState({ userAnswer: true })) }
+            className={ userAnswer ? this.changeAnswer(alternative, crrQuestion) : null }
+            data-testid={ alternative === crrQuestion.correct_answer ? 'correct-answer'
+              : `wrong-answer-${index}` }
           >
-            {alternative === crrQuestion.correct_answer ? (
-              <p data-testid="correct-answer">{alternative}</p>
-            ) : (
-              <p
-                data-testid={ `wrong-answer-${index}` }
-                key={ Math.random() }
-              >
-                {alternative}
-              </p>
-            )}
+            {alternative}
           </button>
         ))}
+        <button
+          type="button"
+          onClick={
+            () => this.setState((prevState) => (
+              { indice: prevState.indice + 1, userAnswer: false }
+            ))
+          }
+        >
+          Próximo
+        </button>
         <Link to="/">
           <button type="button">voltar</button>
         </Link>
@@ -74,9 +95,10 @@ class Game extends React.Component {
   }
 
   render() {
-    const { loading } = this.state;
+    const { loading, indice } = this.state;
+    const limite = 4;
     return (
-      loading ? <p>Loading...</p> : this.renderQuestions()
+      loading || indice > limite ? <p>Loading...</p> : this.renderQuestions()
     );
   }
 }
