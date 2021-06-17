7539fffb3e0222b41efda31f552ea74d303b8cbd
renancosta21 <renancosta21@gmail.com>
2021-06-17 19:09:25 -0300

    fix: lint issues, second try

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import FeedbackMsg from '../components/FeedbackMsg';
-import FeedbackResult from '../components/FeedbackResult';
 
 class Feedback extends Component {
   render() {
@@ -10,7 +9,6 @@ class Feedback extends Component {
       <div>
         <Header />
         <FeedbackMsg />
-        <FeedbackResult />
         <Link
           to="/"
           data-testid="btn-play-again"
