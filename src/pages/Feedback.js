2607d238b946f909b8f21da02b879806c07a5ded
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-16 21:04:09 -0400

    req 14 estruturado

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,11 +3,41 @@ import propTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  renderLessThenThree() {
+    return (
+      <>
+        Podia ser melhor...
+      </>
+    );
+  }
+
+  renderThreeOrMore() {
+    return (
+      <>
+        Mandou bem!
+      </>
+    );
+  }
+
   render() {
     const { history } = this.props;
+    const result = 3;
+    const placar = 45;
+    const three = 3;
     return (
       <div>
         <Header />
+        <main>
+          <h2 data-testid="feedback-text">
+            { result < three ? this.renderLessThenThree() : this.renderThreeOrMore() }
+          </h2>
+          <h3 data-testid="feedback-total-score">
+            { `Placar: ${placar}` }
+          </h3>
+          <h3 data-testid="feedback-total-question">
+            { `Acertos: ${result}` }
+          </h3>
+        </main>
         <button
           type="button"
           data-testid="btn-play-again"
