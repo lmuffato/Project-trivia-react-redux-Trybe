0144b23c9fb6c6afe19463723529246a83381a33
AdelinoJnr <adelinojunior96@gmail.com>
2021-06-17 13:45:35 -0300

    feat: req14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -10,12 +10,25 @@ class Feedback extends Component {
     const { name, email } = user;
     const hash = md5(email).toString();
     const storage = JSON.parse(localStorage.state);
+    const { score, assertions } = storage.player;
     return (
-      <header>
-        <img src={ `https://www.gravatar.com/avatar/${hash}.png` } alt="Gravatar" data-testid="header-profile-picture" />
-        <p data-testid="header-player-name">{`Jogador ${name}`}</p>
-        <p data-testid="header-score">{ storage.player.score }</p>
-      </header>
+      <>
+        <header>
+          <img src={ `https://www.gravatar.com/avatar/${hash}.png` } alt="Gravatar" data-testid="header-profile-picture" />
+          <p data-testid="header-player-name">{`Jogador ${name}`}</p>
+          <p data-testid="header-score">{ score }</p>
+        </header>
+        <section>
+          <p>
+            Placar
+            <span data-testid="feedback-total-score">{ score }</span>
+          </p>
+          <p>
+            Acertos
+            <span data-testid="feedback-total-question">{ assertions }</span>
+          </p>
+        </section>
+      </>
     );
   }
 }
