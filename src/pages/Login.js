02034dab626b0bca149582a42087c7577f13d4df
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-17 17:00:44 -0400

    retirado import desnecessario

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../App.css';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import trivia from '../trivia.png';
 import { login, fetchToken } from '../actions';
