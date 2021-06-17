801f47cdccbb6760a8608eb724264ad7d2636378
Perycles reis <perycles_reis@hotmail.com>
2021-06-16 21:54:01 -0300

    requirement 15 and 16

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
 import FeedBack from './pages/FeedBack';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
     <Switch>
       <Route path="/feedback" component={ FeedBack } />
+      <Route path="/ranking" component={ Ranking } />
       <Route path="/settings" component={ Settings } />
       <Route path="/game" component={ Game } />
       <Route exact path="/" component={ Login } />
