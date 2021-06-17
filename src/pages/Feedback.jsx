999b4c927806ac4d448f2ab9176a33d1656f25f7
igson <igson660@gmail.com>
2021-06-16 22:24:37 -0500

    feat: increment req12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
     return (
       <>
+        <Header />
         <h1 data-testid="feedback-text">
           Feedback
         </h1>
