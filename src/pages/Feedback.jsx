058a45b1df5710ce93e75fd12d7f505d799d946e
AdelinoJnr <adelinojunior96@gmail.com>
2021-06-17 18:08:02 -0300

    fix: req16

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -40,6 +40,14 @@ class Feedback extends Component {
             Jogar novamente
           </button>
         </Link>
+        <Link to="/ranking">
+          <button
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ver Ranking
+          </button>
+        </Link>
       </>
     );
   }
