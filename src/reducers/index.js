4aa5a7dadca7a241c69c54881d37293a25521374
guidugaich <guidugaich.dev@gmail.com>
2021-06-17 17:25:42 -0300

    feat: add timer to rootreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import questions from './questions';
+import timer from './timer';
 
 const rootReducer = combineReducers({
   questions,
+  timer,
 });
 
 export default rootReducer;
