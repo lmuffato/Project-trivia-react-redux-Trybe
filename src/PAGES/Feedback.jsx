243edec059a1a1c2f58ad2cdb00f3832cea10622
hheloisa <hhackenhaar@gmail.com>
2021-06-17 15:13:23 -0300

    inicia estrutura dos req. 13 e 14

--- src/PAGES/Feedback.jsx
+++ src/PAGES/Feedback.jsx
@@ -3,10 +3,31 @@ import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+/*     const { correctAnswers } = this.props;
+    const message = '';
+    if (correctAnswers < 3) {
+
+    } else {
+
+    } */
     return (
-      <Header />
+      <div>
+        <Header />
+{/*         <p data-testid="feedback-text">{ message }</p>
+        <div>
+          <p>Placar final</p>
+          <p>Total de pontos: $</p>
+          <p data-testid="feedback-total-score">{totalScore}</p>
+          <p>Total de acertos:</p>
+          <p data-testid="feedback-total-question">{correctAnswers}</p>
+        </div> */}
+      </div>
     );
   }
 }
+
+/* const mapStateToProps = (state) => ({
+  // correctAnswers: state.Player.questions,
+}); */
 
 export default Feedback;
