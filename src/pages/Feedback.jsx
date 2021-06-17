af9ef2fce92abbffb7bfb774b1cc27c33fade9e7
lucas-paz <lucas.hpaz@outlook.com>
2021-06-17 16:01:32 -0300

    add req 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { history: { location: { state: { hits } } } } = this.props;
+    const minHits = 3;
     return (
       <div>
-        Feedback
+        <Header />
+        <p data-testid="feedback-text">
+          { hits < minHits ? 'Podia ser melhor...' : 'Mandou bem!' }
+        </p>
       </div>
     );
   }
 }
 
-// Feedback.propTypes = {
-
-// };
+Feedback.propTypes = {
+  hits: PropTypes.number,
+}.isRequired;
 
 export default Feedback;
