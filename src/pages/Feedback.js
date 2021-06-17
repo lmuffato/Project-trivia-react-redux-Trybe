14b06881e1058916f73f73c99eeb77783701dfd5
Robertson <robertson.nunes@gmail.com>
2021-06-17 17:53:55 -0300

    main-group-10-requirement14

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,61 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/gameHeader';
 
-const Feedback = () => (
-  <header>
-    <h1 data-testid="feedback-text">Tela Feedback</h1>
-    <Header />
-  </header>
-);
+class Feedback extends React.Component {
+  constructor() {
+    super();
 
-export default Feedback;
+    this.feedbackMessage = this.feedbackMessage.bind(this);
+    this.getAssertions = this.getAssertions.bind(this);
+  }
+
+  getAssertions() {
+    const score = localStorage.getItem('state');
+    const results = JSON.parse(score);
+    const { assertions } = results.player;
+    return assertions;
+  }
+
+  feedbackMessage() {
+    const score = localStorage.getItem('state');
+    const results = JSON.parse(score);
+    const { assertions } = results.player;
+    const maxAssertions = 3;
+    if (assertions < maxAssertions) {
+      return <h1 data-textid="feedback-text">Podia ser melhor...</h1>;
+    }
+    return <h1 data-textid="feedback-text">Mandou bem!</h1>;
+  }
+
+  render() {
+    const { score } = this.props;
+    return (
+      <header>
+        <Header />
+        {this.feedbackMessage()}
+        <h3 data-textid="feedback-total-score">
+          Pontuação Total:
+          {' '}
+          {score}
+          {' '}
+        </h3>
+        <h3 data-textid="feedback-total-question">
+          Total de Acertos:
+          {' '}
+          {this.getAssertions()}
+        </h3>
+      </header>);
+  }
+}
+
+const mapStateToProps = (state) => ({
+  score: state.game.placar,
+});
+
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
