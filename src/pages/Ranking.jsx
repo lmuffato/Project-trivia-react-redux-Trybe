1d6b32a4f5d4b2d4df81a52adca04b5d42afa08c
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-16 21:17:49 -0400

    req 17 - pronto

---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+class Ranking extends Component {
+  render() {
+    const { history } = this.props;
+    return (
+      <main>
+        <h1>Ranking</h1>
+        <button
+          type="button"
+          onClick={ history.push('/') }
+        >
+          Jogar!
+        </button>
+      </main>
+    );
+  }
+}
+
+Ranking.propTypes = {
+  history: propTypes.shape(),
+}.isRequired;
+
+export default Ranking;
