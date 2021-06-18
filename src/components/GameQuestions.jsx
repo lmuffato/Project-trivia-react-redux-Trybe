17252205ca8ae9243aabcd22be56fd3ef889632c
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-06-18 19:54:10 -0300

    fix req 7

--- src/components/GameQuestions.jsx
+++ src/components/GameQuestions.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router';
 import { setQuestions } from '../redux/actions/setQuestions';
+import './GameQuestion.css';
 
 class GameQuestions extends Component {
   constructor(props) {
