ad4364b4b60d0ba3cb1ee4c3507a14ddb3c6ac06
guidugaich <guidugaich.dev@gmail.com>
2021-06-18 00:07:57 -0300

    feat: adding logic to update score

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -8,7 +8,6 @@ class Timer extends Component {
     super();
     this.state = {
       seconds: 30,
-      timerID: '',
     };
     this.decrease1Second = this.decrease1Second.bind(this);
     this.startCountdown = this.startCountdown.bind(this);
@@ -19,27 +18,32 @@ class Timer extends Component {
   }
 
   decrease1Second() {
-    const { props: { stopCountdown, saveSeconds } } = this;
+    const { props: {
+      saveSecondsToGlobal,
+      stopCountdown,
+      addBorderOnClick,
+      disableAlternativeButtons } } = this;
     this.setState(
       (prevState) => ({ seconds: prevState.seconds - 1 }),
       () => {
-        const { state: { seconds, timerID } } = this;
+        const { state: { seconds } } = this;
         // salvar seconds no redux
-        saveSeconds(seconds);
+        saveSecondsToGlobal(seconds);
 
         if (seconds === 0) {
-          stopCountdown(timerID);
+          stopCountdown();
+          addBorderOnClick();
+          disableAlternativeButtons();
         }
       },
     );
   }
 
   startCountdown() {
-    const { props: { saveTimerID } } = this;
+    const { props: { saveTimerIDToGlobal } } = this;
     const ONE_SECOND = 1000;
     const timerID = setInterval(() => this.decrease1Second(), ONE_SECOND);
-    this.setState({ timerID }); // local state
-    saveTimerID(timerID); // global redux state
+    saveTimerIDToGlobal(timerID); // global redux state
   }
 
   render() {
@@ -53,13 +57,14 @@ class Timer extends Component {
 }
 
 const mapDispatchToProps = () => (dispatch) => ({
-  saveTimerID: (timerID) => dispatch(setTimerID(timerID)),
-  saveSeconds: (seconds) => dispatch(saveSeconds(seconds)),
+  saveTimerIDToGlobal: (timerID) => dispatch(setTimerID(timerID)),
+  saveSecondsToGlobal: (seconds) => dispatch(saveSeconds(seconds)),
 });
 
 Timer.propTypes = {
   stopCountdown: PropTypes.func,
-  saveTimerID: PropTypes.func,
+  saveTimerIDToGlobal: PropTypes.func,
+  saveSecondsToGlobal: PropTypes.func,
 }.isRequired;
 
 export default connect(null, mapDispatchToProps)(Timer);
