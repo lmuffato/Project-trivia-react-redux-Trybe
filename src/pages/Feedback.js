b9833b2e0a6d489c55906debe6e4f1b08a0428e6
madruga665 <madruga665@hotmail.com>
2021-06-18 15:00:33 -0300

    Requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
+    const Rate = 3;
     return (
       <div>
         <Header />
-        <h1 data-testid="feedback-text">Feedback</h1>
+        <h1 data-testid="feedback-text">
+          {assertions >= Rate ? 'Mandou bem!' : 'Podia ser melhor...'}
+        </h1>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
