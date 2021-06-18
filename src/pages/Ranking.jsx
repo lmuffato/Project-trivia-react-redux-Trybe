1c2d1beb1fb32d57267aa6f972b4a12d00a9d2ed
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-18 16:11:17 -0400

    adicionado Header

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Ranking extends React.Component {
   render() {
     const { history } = this.props;
     return (
-      <main>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <button
-          type="button"
-          data-testid="btn-go-home"
-          onClick={ () => history.push('/') }
-        >
-          Jogar!
-        </button>
-      </main>
+      <>
+        <Header />
+        <main>
+          <h1 data-testid="ranking-title">Ranking</h1>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ () => history.push('/') }
+          >
+            Jogar!
+          </button>
+        </main>
+      </>
     );
   }
 }
