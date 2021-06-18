f3ad2d6be55ca6e2f5f628d96b1091234e3e0f9f
Alexandre Oliveira <alexandremolive@gmail.com>
2021-06-18 17:07:38 -0300

    correcoes pagina ranking lint

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 import Ranking from './pages/Ranking';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Route path="/game" component={ Game } />
       <Route path="/settings" component={ Settings } />
       <Route path="/ranking" component={ Ranking } />
+      <Route path="/feedback" component={ Feedback } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
