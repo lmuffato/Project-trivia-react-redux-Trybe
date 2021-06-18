c8915cb1504c62e6a23489646cd2fb98e82de816
Marina Alane <marina.alane@outlook.com>
2021-06-18 17:29:51 -0300

    botao de inicio

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // informações renderizadas a partir desempenho no game.
 class Ranking extends React.Component {
   render() {
     return (
       <div>
-        <h1 data-testid="ranking-title"> TELA DE RANKING</h1>
+        <h1 data-testid="ranking-title"> RANKING</h1>
+
+        <Link to="/">
+          <button
+            type="button"
+          >
+            Início
+          </button>
+        </Link>
       </div>
     );
   }
