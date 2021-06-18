b51b3c21ee3699ff41982a99d22710e615ed1f3e
Fernando Castello Branco <fernandoresende@MacBook-Pro-de-Fernando.local>
2021-06-18 17:01:30 -0300

    css do login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,7 @@ import { func } from 'prop-types';
 import { userValidation } from '../utils/functions';
 import retrieveData from '../utils/api';
 import { userGamer } from '../redux/actions';
+import '../styles/login.css';
 
 class Login extends Component {
   constructor() {
@@ -38,47 +39,49 @@ class Login extends Component {
     const { user, email, toRedirect } = this.state;
     return (
       <section>
-        <form>
-          <label htmlFor="user">
-            User:
-            <input
-              onChange={ this.handleChange }
-              data-testid="input-player-name"
-              value={ user }
-              type="text"
-              name="user"
-              id="user"
-            />
-          </label>
-          <label htmlFor="email">
-            Email:
-            <input
-              onChange={ this.handleChange }
-              data-testid="input-gravatar-email"
-              value={ email }
-              type="email"
-              name="email"
-              id="email"
-            />
-          </label>
-          <button
-            disabled={ userValidation(this.state) }
-            data-testid="btn-play"
-            type="button"
-            onClick={ this.handleClick }
-          >
-            Jogar
-          </button>
-        </form>
-        { toRedirect ? <Redirect to="/Game" /> : null }
-        <Link to="/settings">
-          <button
-            type="button"
-            data-testid="btn-settings"
-          >
-            Configurações
-          </button>
-        </Link>
+        <div className="main-container">
+          <form>
+            <label htmlFor="user">
+              User:
+              <input
+                onChange={ this.handleChange }
+                data-testid="input-player-name"
+                value={ user }
+                type="text"
+                name="user"
+                id="user"
+              />
+            </label>
+            <label htmlFor="email">
+              Email:
+              <input
+                onChange={ this.handleChange }
+                data-testid="input-gravatar-email"
+                value={ email }
+                type="email"
+                name="email"
+                id="email"
+              />
+            </label>
+            <button
+              disabled={ userValidation(this.state) }
+              data-testid="btn-play"
+              type="button"
+              onClick={ this.handleClick }
+            >
+              Jogar
+            </button>
+          </form>
+          { toRedirect ? <Redirect to="/Game" /> : null }
+          <Link to="/settings">
+            <button
+              type="button"
+              data-testid="btn-settings"
+            >
+              Configurações
+            </button>
+          </Link>
+        </div>
       </section>
     );
   }
