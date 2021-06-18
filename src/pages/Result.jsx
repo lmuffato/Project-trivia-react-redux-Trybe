62ef0a30aa16ac55e7f67f7ac5882146db04d44d
LucasMLara <lucasml@live.com>
2021-06-18 13:06:47 -0300

    Requisito 12 implementado

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Result extends Component {
   render() {
     return (
       <div data-testid="feedback-text">
+        <Header />
         Result
       </div>
     );
