6572ce4513bd7e3baa25c620ae5fcc745607f3fc
Apoc35 <alexv_silveira@outlook.com>
2021-06-18 14:41:24 -0300

    Requisitos 17 e 18.

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
+  orderMyArray(playerRank) {
+    playerRank.sort((a, b) => b.score - a.score);
+  }
+
   render() {
+    const playerRank = JSON.parse(localStorage.getItem('ranking'));
+    this.orderMyArray(playerRank);
     return (
-      <div data-testid="ranking-title">Meu Ranking</div>
+      <div>
+        <h1 data-testid="ranking-title">Meu Ranking</h1>
+        { playerRank.map((player, index) => (
+          <p key={ `player-name-${index}` }>
+            <span data-testid={ `player-name-${index}` }>
+              { player.name }
+            </span>
+            <span> - </span>
+            <span data-testid={ `player-score-${index}` }>
+              { player.score }
+            </span>
+          </p>
+        ))}
+        <Link to="/">
+          <button type="button" data-testid="btn-go-home">Voltar ao início </button>
+        </Link>
+      </div>
     );
   }
 }
