1962842ff8f7137ec6003b5da8d48fc4b2122396
guidugaich <guidugaich.dev@gmail.com>
2021-06-19 19:10:57 -0300

    feat: creating list with sorted ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    const sortedRanking = ranking.sort((a, b) => b.score - a.score);
+
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <ol>
+          {sortedRanking.map((player, index) => (
+            <li key={ index }>
+              <p data-testid={ `player-name-${index}` }>{player.name}</p>
+              <p data-testid={ `player-score-${index}` }>{player.score}</p>
+              <img src={ player.picture } alt="player-avatar" />
+            </li>
+          ))}
+        </ol>
+        <Link data-testid="btn-go-home" to="/">Jogar Novamente</Link>
       </div>
     );
   }
