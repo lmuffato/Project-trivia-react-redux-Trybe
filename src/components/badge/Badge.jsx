21855731f0db2760fd72c4e2387b56a956370167
WillianMendes <willianmendesbr@gmail.com>
2021-06-20 19:50:26 -0300

    feat: estilizacao do header

---
+++ src/components/badge/Badge.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BadgeCustom from './BadgeStyle';
+
+class Badge extends Component {
+  render() {
+    const { dataTestId, text, value, classIcon, classList } = this.props;
+
+    return (
+      <BadgeCustom className={ classList }>
+        <span>
+          {text}
+        </span>
+        <span data-testid={ dataTestId }>{value}</span>
+      </BadgeCustom>
+    );
+  }
+}
+
+Badge.propTypes = {
+  text: PropTypes.string.isRequired,
+  classList: PropTypes.string.isRequired,
+  dataTestId: PropTypes.string,
+  classIcon: PropTypes.string,
+  value: PropTypes.string.isRequired,
+};
+
+Badge.defaultProps = {
+  dataTestId: '',
+  classIcon: '',
+};
+
+export default Badge;
---
+++ src/components/badge/BadgeStyle.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const BadgeCustom = styled.div`
+  color: var(--color-white);
+  margin: 4px 0;
+  padding: 8px 12px;
+  border-radius: 8px;
+  
+  &.badge-primary {
+    background-color: var(--color-primary);
+  }
+
+  &.badge-secondary {
+    background-color: var(--color-secondary);
+  }
+  
+  span:first-child {
+    font-weight: 700;
+  }
+  
+  span:last-child {
+    margin-left: 8px;
+  }
+`;
+
+export default BadgeCustom;
