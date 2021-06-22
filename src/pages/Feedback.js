e02242994abdf7376a28fc2c94010be6a7372b34
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-06-22 13:37:36 -0300

    Feat: Adding feedback screen.

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
     return (
       <div>
         <p data-testid="feedback-text">Feedback</p>
+        <Header />
       </div>
     );
   }
