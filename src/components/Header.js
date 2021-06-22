e96069a6935718fa0c1dc37f46382dc4ece597ac
Marcus C. H. Filho <mchusemann@gmail.com>
2021-06-21 22:46:30 -0300

    feat: adiciona gif, fontes e sombras

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,19 @@ class Header extends Component {
     const { userName, userEmail, userScore } = this.props;
     return (
       <div className="header">
-        <img
-          src={ `https://www.gravatar.com/avatar/${md5(userEmail)}` }
-          alt="Sua Foto do Gravatar"
-          data-testid="header-profile-picture"
-        />
+        <div className="ctn-user-img">
+          <img
+            className="user-img"
+            src={ `https://www.gravatar.com/avatar/${md5(userEmail)}` }
+            alt="Sua Foto do Gravatar"
+            data-testid="header-profile-picture"
+          />
+        </div>
+        <div className="ctn-squirrel">
+          <h1 className="title">Shipit Trivia Game</h1>
+
+          <img className="squirrel" src="https://66.media.tumblr.com/tumblr_maw6nhSaKa1rfjowdo1_500.gif" alt="squirrel-gif" />
+        </div>
         <div className="header-user-info">
           <p className="title is-4" data-testid="header-player-name">{ userName }</p>
           <p className="subtitle is-2" data-testid="header-score">{ userScore }</p>
